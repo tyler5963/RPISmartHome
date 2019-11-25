78f8d13e42bb540a5c7d5ad23f060cdd725b4f45
djcicala <darrenjc99@hotmail.com>
2019-11-25 12:30:01 -0500

    11_25 temp sensor read and config changes

--- configs.py
+++ configs.py
@@ -1,17 +1,47 @@
 '''****************************************************************************
 * File Name: configs.py                                                       *
 * Purpose:   Global variables and configurations for RPI Smart Home.          *
-* Date:      11/17/2019                                                       *
+* Date:      11/24/2019                                                       *
 * Copyright © 2019 Darren Cicala and Tyler Skene. All rights reserved.        *
 ****************************************************************************'''
 
 # document version
-__version__ = "0.1.0"
+__version__ = "0.2.0"
+
+# there should be no imports in this file!
+
+# global switches
+SW_USE_METRIC_UNITS = False
+
+# error codes
+SYSERROR_NO_ERROR                    = 0
+SYSERROR_TEMP_OUTDOOR_SENSOR_FAILURE = 1
+SYSERROR_TEMP_INDOOR_SENSOR_FAILRUE  = 2
+SYSERROR_WL_MOISTURE_SENSOR_FAILURE  = 3
+SYSERROR_WL_API_CALL_FAILURE         = 4
+SYSERROR_GUI_GENERAL_FAILURE         = 5
 
 # API information
 s_ApiBase            = "https://api.darksky.net/forecast/"     # root API 
 s_ApiKey             = "" # modify this with your own darksky key
 s_GPSLocation_LatLon = "" # modify this with your own GPS coordinates, e.g. "37.5148,15.7891"
 s_FullAPI = s_ApiBase + s_ApiKey +  "/" + s_GPSLocation_LatLon # full API path
+
+# temperature sensor information
+i_IndoorSensorPin  = 17   # modify this with your own sensor signal pin for the outside
+i_OutdoorSensorPin = 16   # modify this with your own sensor signal pin for indoors
+
+i_DoNothingFlag    = 0
+i_HeatFlag         = 1
+i_CoolFlag         = 2
+
+# moisture sensor information
+# note: to enable SPI for you:
+# 1. run sudo raspi-config
+# 2. go to interfacing options
+# 3. go to SPI, press enter to enable
+# check to see if /dev/spidev0.0 exists 
+i_SPIDevice = 0
+i_SPIPort   = 0
 
 ################################## end file ###################################
--- temperature.py
+++ temperature.py
@@ -6,9 +6,52 @@
 ****************************************************************************'''
 
 # document version
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 # imports
-import configs
+import configs      # global configs file
+import Adafruit_DHT # library to interface with DHT11 sensor
 
+class TemperatureSensor:
+	
+	'''*****************************************************************
+	* Name: __init__                                                                  
+	* Description: Constructor for class TemperatureSensor                      
+	* Parameters:  int i_InputGPIOPin                                              
+	*                    GPIO pin on the Pi that the sensor is wired to.                      
+	* Returns:     N/A                     
+	*****************************************************************'''
+	def __init__(i_InputGPIOPin):
+		self.i_SensorType = Adafruit_DHT.DHT11 # type of sensor
+		self.i_GPIOPin    = i_InputGPIOPin     # what pin the sensor is connected to
+		self.i_ErrorFlag  = configs.SYSERROR_NO_ERROR # shocker! we don't have any errors because we just initialized it
+	
+	'''*****************************************************************
+	* Name: read                                                                  
+	* Description: Function to read from the sensor, and update class members.                      
+	* Parameters:  N/A                     
+	* Returns:     N/A (modifies class members)                 
+	*****************************************************************'''	
+	def read():
+		# update "previous" values
+		self.f_HumidityPrev_Pct  = self.f_Humidity_Pct
+		self.f_TemperaturePrev_C = self.f_Temperature_C
+		self.f_TemperaturePrev_F = self.f_Temperature_F
+		
+		# get new values 
+		self.f_Humidity_Pct, self.f_Temperature_C = Adafruit_DHT.read_retry(sensor_name, sensor_pin)
+		self.f_Temperature_F = ((9/5) * self.f_Temperature_C) + 32
+		
+	'''*****************************************************************
+	* Name: MakeHVACDecision                                                                  
+	* Description: Makes a decision to cool or warm the house based on
+	*              the external temperature of the system. This function
+	*              should only be called for the external temperature sensor.                      
+	* Parameters:  N/A                     
+	* Returns:     ??                 
+	*****************************************************************'''
+    def MakeHVACDecision():
+	    # case 1: external temp less than 65 degrees 
+	    if(
+    def ChangeSystemState():		
 ################################## end file ###################################
