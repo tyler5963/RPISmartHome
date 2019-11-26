86522ff30b129f359834c36315042709baf78172
djcicala <darrenjc99@hotmail.com>
2019-11-26 17:13:03 -0500

    11_26_4 new file, systime.py

---
+++ Source/systime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''****************************************************************************
+* File Name: systime.py                                                       *
+* Purpose:   Methods and functions pertaining to timekeeping.                 *
+* Date:      11/26/2019                                                       *
+* Copyright © 2019 Darren Cicala and Tyler Skene. All rights reserved.        *
+* Powered by the DarkSky API.                                                 *
+****************************************************************************'''
+
+# document version
+__version__ = "0.1.0"
+
+# imports
+import configs
+
+################################## end file ###################################
